d83dcdfb7db2e49f5c58cc2ae1902ba6056aa8f3
luoly <https://github.com/luoly2015/apple.git>
2018-04-16 18:39:13 +0800

    dugujiujian commit

--- src/main/java/Huashan.java
+++
@@ -1,7 +0,0 @@
-
-public class Huashan {
-
-	public void huashanjianfa() {
-		System.out.println("华山剑法天下第一！");//hello master edit by luoly
-	}
-}
---
+++ src/main/java/xajh/Dugu9jian.java
@@ -0,0 +1,8 @@
+package xajh;
+
+public class Dugu9jian {
+
+	public void dugu() {
+		System.out.println("独孤九剑天下无敌！");
+	}
+}
