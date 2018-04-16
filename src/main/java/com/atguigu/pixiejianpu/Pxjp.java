532c956ac4107c3bd4cb0fe24ec6563873455fef
luoly <https://github.com/luoly2015/apple.git>
2018-04-16 19:44:46 +0800

    核心武学研究1.0

--- src/main/java/com/atguigu/pixiejianpu/Pxjp.java
+++ src/main/java/com/atguigu/pixiejianpu/Pxjp.java
@@ -2,6 +2,6 @@ package com.atguigu.pixiejianpu;
 
 public class Pxjp {
     public void pxjp(){
-        System.out.println("此功法威力极大！");
+        System.out.println("此功法威力极大！");//好好研究，作为华山门派至高绝学流传！
     }
 }
