54c66e2e7ebb4c0b9fb34effceaa1db7c881ce79
luoly <https://github.com/luoly2015/apple.git>
2018-04-16 19:48:59 +0800

    确认令狐冲峨眉掌门

--- src/main/java/com/atguigu/emeijf/Emeijf.java
+++ src/main/java/com/atguigu/emeijf/Emeijf.java
@@ -4,4 +4,7 @@ public class Emeijf {
     public void Emjf(){
         System.out.println("峨眉剑法博大精深！");//这将是一项伟大的剑法
     }
+    public void zhangmen(){
+        System.out.println("令狐冲第一任峨眉掌门!");
+    }
 }
