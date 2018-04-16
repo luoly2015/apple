4292cb8890bdd671e4b397d80f2bdb3d34904187
luoly <https://github.com/luoly2015/apple.git>
2018-04-16 19:20:17 +0800

    emjf edit by luoly,test!

--- src/main/java/com/atguigu/emeijf/Emeijf.java
+++ src/main/java/com/atguigu/emeijf/Emeijf.java
@@ -2,6 +2,6 @@ package com.atguigu.emeijf;
 
 public class Emeijf {
     public void Emjf(){
-        System.out.println("峨眉剑法博大精深！");
+        System.out.println("峨眉剑法博大精深！");//这将是一项伟大的剑法
     }
 }
